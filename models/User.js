029f312d55de52b16db683fa63c22d02210ead2f
kumardheeraj9311 <kumardheeraj9311@gmail.com>
2025-11-19 00:39:13 +0530

    usermodel

--- models/User.js
+++ models/User.js
@@ -4,7 +4,9 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   name: { type: String },
   password: { type: String, required: true },
-  avatar: { type: String, default: null }
+  avatar: { type: String, default: null },
+  wallet: { type: Number, default: 0 },
+  bio: { type: String, default: '' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
