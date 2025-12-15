dcd37eb4dbbd52065e9e5f19f360fa3555903c39
kumardheeraj9311 <kumardheeraj9311@gmail.com>
2025-12-15 15:57:17 +0530

    commentssoooo

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '7d';
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '100Y';
 
 function generateToken(user) {
   return jwt.sign(
